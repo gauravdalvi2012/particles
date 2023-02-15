c1becd2153f5aaa7b623aa06c56fd9619e79f3d9
Gaurav Dalvi <gaurav.wizwack@gmail.com>
2023-02-15 23:35:27 +0530

    added touchmove

--- script.js
+++ script.js
@@ -26,7 +26,15 @@ canvas.addEventListener('click', function (event) {
 canvas.addEventListener('mousemove', function (event) {
     mouse.x = event.x;
     mouse.y = event.y;
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 2; i++) {
+        particlesArray.push(new Particle());
+    }
+});
+
+canvas.addEventListener('touchmove', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for (let i = 0; i < 2; i++) {
         particlesArray.push(new Particle());
     }
 });
