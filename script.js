b801e9f3b83bdcd6829282f022228faf0cbe8a19
Gaurav Dalvi <gaurav.wizwack@gmail.com>
2023-02-15 23:23:35 +0530

    particles

---
+++ script.js
@@ -0,0 +1,92 @@
+const canvas = document.getElementById('canvas1');
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particlesArray = [];
+let hue = 0;
+
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+const mouse = {
+    x: undefined,
+    y: undefined,
+}
+
+canvas.addEventListener('click', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for (let i = 0; i < 10; i++) {
+        particlesArray.push(new Particle());
+    }
+});
+
+canvas.addEventListener('mousemove', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for (let i = 0; i < 1; i++) {
+        particlesArray.push(new Particle());
+    }
+});
+
+class Particle {
+    constructor() {
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.size = Math.random() * 16 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.color = 'hsl(' + hue + ',100%, 50%)';
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.size > 0.2) {
+            this.size -= 0.1;
+        }
+    }
+
+    draw() {
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        context.fill();
+    }
+}
+
+function handleParticles() {
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        particlesArray[i].draw();
+        for (let j = i; j < particlesArray.length; j++) {
+            const dx = particlesArray[i].x - particlesArray[j].x;
+            const dy = particlesArray[i].y - particlesArray[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < 100) {
+                context.beginPath();
+                context.strokeStyle = particlesArray[i].color;
+                context.lineWidth = 0.5;
+                context.moveTo(particlesArray[i].x, particlesArray[i].y);
+                context.lineTo(particlesArray[j].x, particlesArray[j].y);
+                context.stroke();
+                context.closePath();
+            }
+        }
+        if (particlesArray[i].size <= 0.3) {
+            particlesArray.splice(i, 1);
+            i--;
+        }
+    }
+}
+
+function animate() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    hue += 2;
+    requestAnimationFrame(animate);
+}
+
+animate();
